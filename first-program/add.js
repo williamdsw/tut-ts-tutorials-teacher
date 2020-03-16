babd90cd18ec48293d5fce7bbccb262ae0336571
williamdsw <williamdsw@outlook.com>
2020-03-16 20:58:48 -0300

    First Program

---
+++ first-program/add.js
@@ -0,0 +1,5 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+var sum = addNumbers(10, 15);
+console.log('10 + 15 = ', sum);
---
+++ first-program/add.ts
@@ -0,0 +1,7 @@
+
+function addNumbers(a: number, b: number) {
+    return a + b;
+}
+
+var sum: number = addNumbers (10, 15);
+console.log ('10 + 15 = ', sum);
