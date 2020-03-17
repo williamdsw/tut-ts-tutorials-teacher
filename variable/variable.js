9eb67162ee755bec458ab948a731d9c5afaf3897
williamdsw <williamdsw@outlook.com>
2020-03-16 21:20:55 -0300

    Variable

---
+++ variable/variable.js
@@ -0,0 +1,6 @@
+var firstName = 'David';
+var lastName = 'Gilmour';
+var BIRTHDATE = new Date(1946, 2, 6);
+console.log('firstName:', firstName);
+console.log('lastName:', lastName);
+console.log('BIRTHDATE:', BIRTHDATE);
---
+++ variable/variable.ts
@@ -0,0 +1,8 @@
+
+var firstName: string = 'David';
+let lastName: string = 'Gilmour';
+const BIRTHDATE: Date = new Date (1946, 2, 6);
+
+console.log ('firstName:', firstName);
+console.log ('lastName:', lastName);
+console.log ('BIRTHDATE:', BIRTHDATE);
