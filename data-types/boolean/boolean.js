3c1a03e6921f7046897e44f243285f20190dccad
williamdsw <williamdsw@outlook.com>
2020-03-16 21:40:17 -0300

    Data Types -> Boolean

---
+++ data-types/boolean/boolean.js
@@ -0,0 +1,6 @@
+var isPresent = true;
+function checkExistence(existence) {
+    return existence;
+}
+console.log('Is Present ?', isPresent ? 'Yes' : 'No');
+console.log('Exists ?', checkExistence(false) ? 'Yes' : 'No');
---
+++ data-types/boolean/boolean.ts
@@ -0,0 +1,9 @@
+
+let isPresent: boolean = true;
+
+function checkExistence(existence: boolean) {
+    return existence;
+}
+
+console.log ('Is Present ?', isPresent ? 'Yes' : 'No');
+console.log ('Exists ?', checkExistence (false) ? 'Yes' : 'No');
