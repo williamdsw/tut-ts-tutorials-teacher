4bb97f5fd9be857c00bc4a639256a0df5a069ac0
William <williamdsw@outlook.com>
2020-04-13 20:28:03 -0300

    Data Types -> Void

---
+++ data-types/void/void.js
@@ -0,0 +1,5 @@
+function print() {
+    console.log('Into the void');
+}
+var phrase = print();
+console.log('phrase: ', phrase);
---
+++ data-types/void/void.ts
@@ -0,0 +1,7 @@
+
+function print(): void {
+    console.log ('Into the void');
+}
+
+let phrase: void = print();
+console.log ('phrase: ', phrase);
