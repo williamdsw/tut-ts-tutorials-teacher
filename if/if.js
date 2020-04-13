e3be11ba48f59cbc519db0b505e2a9a3e8b54364
William <williamdsw@outlook.com>
2020-04-13 20:56:03 -0300

    IF statement

---
+++ if/if.js
@@ -0,0 +1,19 @@
+var x = 15;
+var y = 20;
+var z = 10;
+console.log('x = ', x, '\ny = ', y, '\nz = ', z);
+if (x < y) {
+    console.log('y is greater than x');
+}
+if (z > y) {
+    console.log('z is greater than y');
+}
+else {
+    console.log('z is not greater than y');
+}
+if (x == z) {
+    console.log('x and z are equals');
+}
+else if (x > z) {
+    console.log('x is greater than z');
+}
