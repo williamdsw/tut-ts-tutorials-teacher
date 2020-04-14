85831630434b0c95a13c3f840213d0f28dc45787
William <williamdsw@outlook.com>
2020-04-14 16:29:25 -0300

    Switch Statement

---
+++ switch/switch.js
@@ -0,0 +1,45 @@
+function printCardGuardian(guardian) {
+    switch (guardian) {
+        case 0: {
+            console.log('SUN');
+            break;
+        }
+        case 1: {
+            console.log('MERCURY');
+            break;
+        }
+        case 2: {
+            console.log('VENUS');
+            break;
+        }
+        case 3: {
+            console.log('MOON');
+            break;
+        }
+        case 4: {
+            console.log('MARS');
+            break;
+        }
+        case 5: {
+            console.log('JUPITER');
+            break;
+        }
+        case 6: {
+            console.log('SATURN');
+            break;
+        }
+        case 7: {
+            console.log('NEPTUNE');
+            break;
+        }
+        case 8: {
+            console.log('URANUS');
+            break;
+        }
+        case 9: {
+            console.log('PLUTO');
+            break;
+        }
+    }
+}
+printCardGuardian(2);
